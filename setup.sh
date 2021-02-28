ed8499aa2edd7712a3dd19624d93cc5c9ef7190b
Johnathan Eshleman <johnathan.eshleman@sas.com>
2021-02-28 15:49:42 -0500

    Add vscode preferences

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ defaults write com.microsoft.VSCode CGFontRenderingFontSmoothingDisabled 0
 defaults write com.microsoft.VSCode.helper CGFontRenderingFontSmoothingDisabled 0
 defaults write com.microsoft.VSCode.helper.EH CGFontRenderingFontSmoothingDisabled 0
 defaults write com.microsoft.VSCode.helper.NP CGFontRenderingFontSmoothingDisabled 0
+
+ln -s ~/Projects/dotfiles/vscode/snippets ~/Library/Application Support/Code/User/snippets
+ln -s ~/Projects/dotfiles/vscode/keybindings.json ~/Library/Application Support/Code/User/keybindings.json
+ln -s ~/Projects/dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
